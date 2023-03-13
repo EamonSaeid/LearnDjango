1131807adff4c135fa1aca7788d0349e9b3e22b4
EamonSaeid <eamonsaeid@msn.com>
2023-03-13 14:06:37 +0000

    first template and view

---
+++ movies/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+data={
+    'movies':[
+        {
+         'id':'5',
+         'title':'Jaws',
+         'year':1669  
+        },
+        {
+         'id':'6',
+         'title':'Django',
+         'year':2010  
+        },
+        {
+         'id':'7',
+         'title':'Sharknado',
+         'year':1980  
+        }]
+}
+def movies(request):
+    return render(request, 'movies/movies.html',data)
+
+def home(request):
+    return HttpResponse("Home Page")
